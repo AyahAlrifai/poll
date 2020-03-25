1fd031f660b39c8803252c876d5b76f3285d0857
Ayah Alrifai <48604304+AyahAlrifai@users.noreply.github.com>
2020-03-26 01:57:48 +0200

    Update poll.js

--- poll.js
+++ poll.js
@@ -143,7 +143,7 @@ var UIController=(function() {
           str+='<div id="v-'+question.id+'-'+choices[i].id+'" class="progress-bar bg-info progress-bar-striped" style="width:'+vote+'%;color:#000000">'+choices[i].choiceHead +"....votes:"+choices[i].vote+'</div></div></div><div class="col-xl-1"><i  class="col-sm-right fas fa-vote-yea vote-choice" id="'+question.id+'-'+choices[i].id+'"></i></div><div class="col-xl-1"></div></div>';
       }
       str+='</div>';
-      document.getElementById(DOMstrings.posts).insertAdjacentHTML('beforeend', str );
+      document.getElementById(DOMstrings.posts).insertAdjacentHTML('afterbegin', str );
 
     },
 
